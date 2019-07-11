0f74a9e5df027ac78ef9b329ebe942a6dead1a65
Simon Bernsdorff Wallstedt <simon.bernsdorff@outlook.com>
2019-07-11 14:50:07 +0200

    what am i doing

--- calculator.js
+++ calculator.js
@@ -1,22 +1,42 @@
 const keys = document.querySelector('.calculator');
-let output = document.getElementById('output');
+let output = document.getElementById('output').textContent;
+let firstvalue = 0;
+let operator = '';
+let operatorPressed = false;
 
 keys.addEventListener('click', function(e) {
-    const action = e.target.classList;
-    if (action.contains("division") || action.contains("multiplie") || action.contains('minus') || action.contains('plus')) {
-      console.log('operator key!');
+    let action = e.target.classList;
+
+    if (action.contains("division")) {
+
+    }
+    else if (action.contains("multiplie")) {
+
+    }
+    else if (action.contains("minus")) {
+
+    }
+    else if (action.contains("plus")) {
+        firstvalue = output.textContent;
+        operator = 'plus';
+        operatorPressed = true;
     }
     else if (action.contains('equals')) {
-        console.log('equals key');
+
     }
     else if (action.contains('clear')) {
         clearDisplay();
     }
     else if (action.contains('dot')) {
-        output.textContent = output.textContent + '.'
+        if (output.textContent.indexOf(".") !== -1) {
+            return
+        }
+        else {
+            output.textContent = output.textContent + ".";
+        }
     }
-    else if (!action.contains('operators')) {
-        if (output.textContent == '0') {
+    else if (action.contains('number')) {
+        if (output.textContent === '0') {
             output.textContent = e.target.textContent;
         }
         else {
